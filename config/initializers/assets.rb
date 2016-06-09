c2202de71f9d5a682ac3c1105c32ad00af1ffde2
JackLin <tkicorp@gmail.com>
2016-06-09 14:26:44 +0800

    add teacher-selfie

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.precompile += %w( method2.jpg )
 Rails.application.config.assets.precompile += %w( method3.jpg )
 Rails.application.config.assets.precompile += %w( method4.jpg )
 Rails.application.config.assets.precompile += %w( image01.png )
+Rails.application.config.assets.precompile += %w( photo/selfie.png )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
